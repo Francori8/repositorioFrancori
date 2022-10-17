111956b0bde6056383a55e58ae9f99e3c4fc4082
Francori8 <francoorizonte@gmail.com>
2022-10-17 18:02:29 -0300

    Actualizacion Mate

--- informacion.js
+++ informacion.js
@@ -86,7 +86,12 @@ const videoM19 = new Videos(19,"ixZCQU24sC0","16/09/2022","Teoria de Conjuntos 1
 const videoM20 = new Videos(20,"zvAaOhuXIuI","23/09/2022","Relaciones 1", "Relaciones Binarias")
 const videoM21 = new Videos(21,"vmXgYNUaxqA","23/09/2022","Relaciones 2","Relacion Inversion")
 const videoM22 = new Videos(22, "tSw7aJ-3hS4","23/09/2022","Relaciones 3", "Composicion de Relacion")
-const videoM23= new Videos(23,"kqJhginNcqw","27/09/2022","Relaciones 4","Relaciones definidas en un conjunto")
+const videoM23= new Videos(23,"kqJhginNcqw","27/09/2022","Relaciones 4","Relaciones definidas en un conjunto, Relaciones de Equivalencia")
+const videoM24 = new Videos(24,"7zd2tHxZuSw","27/09/2022","Relaciones 5","Relaciones de Orden")
+const videoM25 = new Videos(25,"HbUMm7k4hDY","14/10/2022","Funciones 1","Clasificacion de las funciones y varias de ellas")
+const videoM26 = new Videos(26,"X6HDs6MAocc","18/10/2022","Funciones 2","Igualdad de Funciones")
+const videoM27 = new Videos(27,"1CypdaruT5s","21/10/2022","Funciones 3", "Composicion de Funciones")
+const videoM28 = new Videos(28,"u67EEWJIqnM","21/10/2022","Funciones 4","Funcion Inversa")
 
 
 const apunteMT1 = new ApuntesT(1,"11FKyBjjHMXqiwRs5N_zYokG_3fizW4-p","23/08/2022","Logica 1","Logica, Formulas Proposicionales, Leyes Logicas","Teorico",1,1,"1-19")
@@ -102,18 +107,24 @@ const apunteMT10 = new ApuntesT(10,"1MEOkMDL-dPVfceVctZaNlQMwhQN31jTq","16/09/20
 const apunteMT11 = new ApuntesT(11,"1SccCLm5Nzy9UW6qzMrxN4RheA2cvD8Sq","23/09/2022","Relaciones 1","Relaciones Binarias, dominio e imagen","Teorico",[20,21],3,[1,2,3])
 const apunteMT12 = new ApuntesT(12,"1inWCCMPzolu1owndTkfpBcN3UoafsuD4","23/09/2022","Relaciones 2","Composicion de Relaciones","Teorico",22,3,[1,2,3])
 const apunteMT13 = new ApuntesT(13,"13gT6RjIXsslsSnZPI_JZ1dD_d32dVidx","27/09/2022","Relaciones 3","Relacione entre un conjunto","Teorico",23,3,"1,2,3,5")
-const apunteMT14 = new ApuntesT(14,"1drl0zxMPHq6tuu_ntmatO08FwXYzV52I","30/09/2022","Relaciones 4","Relaciones de equivalencias","Teorico",24,3,"")
-const apunteMT15 = new ApuntesT(15,"14o6ZjCH0TWLGhKTSYKlPvWxToLivV3q3","30/09/2022","Relaciones 5","Relaciones de Orden","Teorico",25,3,"")
+const apunteMT14 = new ApuntesT(14,"1drl0zxMPHq6tuu_ntmatO08FwXYzV52I","30/09/2022","Relaciones 4","Relaciones de equivalencias","Teorico",23,3,"")
+const apunteMT15 = new ApuntesT(15,"14o6ZjCH0TWLGhKTSYKlPvWxToLivV3q3","30/09/2022","Relaciones 5","Relaciones de Orden","Teorico",24,3,"")
+const apunteMT16 = new ApuntesT(16,"1IuMHVyO-GHnVMgyRs1rKo2yMP56Dvc9G","14/10/2022","Funciones 1 a ","Funciones, Grafica, Dominio","Teorico",null,4,"1")
+const apunteMT17 = new ApuntesT(17,"1Ck3d3cvsSCrhA0ItPgy1is6Jagttpkht","14/10/2022","Funciones 1 b","Biyectiva, Intectiva y Sobreyectiva","Teorico",25,4,"Todos")
+const apunteMT18 = new ApuntesT(18,"1WZNSFBxw_CsVrlUu87anmiv3GcPxrPYJ","18/10/2022","Funciones 2","Igualdad de funciones, tipos de funciones","Teorico",26,5,"Todos")
+const apunteMT19 = new ApuntesT(19,"1LfPeY_zuMNa_wTCcUsM5qiTuy09rRraU","21/10/2022","Funciones 3","Composicion de Funciones","Teorico",27,"","")
+const apunteMT20 = new ApuntesT(20,"1sd_J_gPSZ8Xn3_yH7gXq2n7LqRihvdIJ","21/10/2022","Funicones 4","Funcion Inversa","Teorico",28,"","")
 
 
 const apunteMP1 = new ApuntesP(1,"12NetAvDgn7barmTUuDTFfJkD5M8Ifz0R/","23/08/2022","Logica TP","Logica /Razonamientos / Formulas Proposicionales","Practico")
 const apunteMP2 = new ApuntesP(2,"1s5rky_xxbQ3SBTHv30Gg-IUU5cIH53Nf","09/09/2022","Conjuntos TP","Teoria de conjuntos / Union / Interseccion","Practico")
 const apunteMP3 = new ApuntesP(3,"1EcjjSxA4_-3S0RSBoNkwCfv9SuLR1pyS","23/09/2022","Relaciones","Relaciones entre conjuntos, binaria","Practico")
+const apunteMP4 = new ApuntesP(4,"18JhaU9aENk9copCSBgA3TcuKkT06YzgH","18/10/2022","Funciones TP","Funciones, composicion, inversa","Practico")
+const apunteMP5 = new ApuntesP(5,"1L_z2Xrgrdpj1tZoZ4knWpxU17sD9-HGT","18/10/2022","Funciones TP 2","Ejercictacion funciones","Practico")
 
-
-matematica1.videos.push(videoM1,videoM2,videoM3,videoM4,videoM5, videoM6,videoM7, videoM8, videoM9, videoM10, videoM11,videoM12,videoM13, videoM14,videoM15, videoM16,videoM17, videoM18,videoM19, videoM20,videoM21,videoM22, videoM23)
-matematica1.apuntesteoricos.push(apunteMT1,apunteMT2,apunteMT3,apunteMT4,apunteMT5,apunteMT6, apunteMT7,apunteMT8,apunteMT9,apunteMT10,apunteMT11,apunteMT12, apunteMT13, apunteMT14, apunteMT15)
-matematica1.apuntespracticos.push(apunteMP1,apunteMP2, apunteMP3)
+matematica1.videos.push(videoM1,videoM2,videoM3,videoM4,videoM5, videoM6,videoM7, videoM8, videoM9, videoM10, videoM11,videoM12,videoM13, videoM14,videoM15, videoM16,videoM17, videoM18,videoM19, videoM20,videoM21,videoM22, videoM23, videoM24, videoM25,videoM26,videoM27,videoM28)
+matematica1.apuntesteoricos.push(apunteMT1,apunteMT2,apunteMT3,apunteMT4,apunteMT5,apunteMT6, apunteMT7,apunteMT8,apunteMT9,apunteMT10,apunteMT11,apunteMT12, apunteMT13, apunteMT14, apunteMT15,apunteMT16, apunteMT17, apunteMT18,apunteMT19, apunteMT20)
+matematica1.apuntespracticos.push(apunteMP1,apunteMP2, apunteMP3, apunteMP4, apunteMP5)
 
 //INTRODUCCIONPROGRAMACION
 const introduccionProgramacion = new Materias("Inpr","Introducción a la programación",8,"1BEQTT_9Lisq-PX6dEAGoclC6Nr1TjGF8","1n0fOy_9yfjjdQorwG4X_ZN1iOUSoO1XK",`<a href ="https://gobstones.github.io/gobstones-jr/?course=gobstones/curso-InPr-UNQ" target = "_blanck">Entorno Gobstone JR</a> ,<a href="https://gobstones.github.io/gobstones-sr/?course=gobstones/curso-InPr-UNQ" target= "_blank">Entorno Gobstone SR</a>`,"De las 8 hrs semanales se vuelven 6 ya que las dos correspondientes a los miercoles, es para que practiques en tu casa, igual se reservan ese dia y horario para que el examen sea ese dia. Examen en papel aunque todas las clases son en computadora, Si no sabes programar hasta parametros es relativamente facil despues de eso se complica un poco, Si sabes programar, lo que mas te va a costar son los contratos, la sintaxis y algunas cosas raras de gobstone. Clases totalmente invertidas, es decir, la forma de la clase es: indagacion, teoria y luego practica, la cual en clase presencial la idea es que sea solo practica, aunque los profesores te responden cualquier consulta que tengas, los contenidos se van desbloqueando semana a semana y tienen un campus virtual distinto al de la facultad (2022 mi caso)")
