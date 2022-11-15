e78df9af4e314ff2ad3fe0bb88e2b51464997d87
Francori8 <francoorizonte@gmail.com>
2022-11-15 19:49:32 -0300

    Agregado Apuntes y videos De Matematica

--- informacion.js
+++ informacion.js
@@ -64,6 +64,8 @@ class Videos{
 // MATEMATICA 1
 const matematica1 = new Materias("Mat1","Matemática 1",8,"1uT1fhQkMv1vXHHdp0v7QHHiUzOy0mOPz","1uT1fhQkMv1vXHHdp0v7QHHiUzOy0mOPz",null,"La materia es complicada, la base de EPyL es bastante util al principio, es mucha abstraccion, tu decide si eso es bueno o malo, aunque casi todos los temas son las bases de materias posteriores. La practica es la parte mas fundamental, ademas que hay ejercicios que no sabras si los estas justificando bien o mal. Las clases son teoricas/practicas, lo mejor para este tipo de materias, ademas del apoyo de los videos y los pdfs son bastantes entendibles, aunque las practicas ya es otro tema, aun asi si la sigues quizas no sea tan dificil (2022 mi caso)")
 
+//Videos
+
 const videoM1 = new Videos(1,"94WQQE-3Fu8","23/08/2022", "Lógica  ","Logica, Formulas Proposicionales, Leyes Logicas")
 const videoM2 = new Videos(2,"bl_8qx4a3J8","30/08/2022", "Logica Razonamiento 1 ","Razonamientos, Explicacion, Formalizacion")
 const videoM3 = new Videos(3,"GoLXyZ4KaZs","30/08/2022", "Logica Razonamiento 2 ","Razonamientos, Metodo Directo")
@@ -92,7 +94,14 @@ const videoM25 = new Videos(25,"HbUMm7k4hDY","14/10/2022","Funciones 1","Clasifi
 const videoM26 = new Videos(26,"X6HDs6MAocc","18/10/2022","Funciones 2","Igualdad de Funciones")
 const videoM27 = new Videos(27,"1CypdaruT5s","21/10/2022","Funciones 3", "Composicion de Funciones")
 const videoM28 = new Videos(28,"u67EEWJIqnM","21/10/2022","Funciones 4","Funcion Inversa")
+const videoM29 = new Videos(29,"VV1wPpi43HI","1/11/2022","Induccion Matematica","Principio de induccion matematica, Suamtoria, Factorial")
+const videoM30 = new Videos(30,"gm5VTLz3S1c","11/11/2022 ","Analisis Combinatorio 1","Concepto de analisis combinatorio y Variaciones")
+const videoM31 = new Videos(31,"m4pIJfL6ScM","11/11/2022","Analisis Combinatorio 2","Permutaciones")
+const videoM32 = new Videos(32,"4TLs-UA9xwY","11/11/2022","Analisis Combinatorio 3","Combinaciones")
+const videoM33 = new Videos(33,"xnrEmtnrAJM","11/11/2022","Analisis Combinatorio 4","Variaciones con repeticiones")
+const videoM34 = new Videos(34,"xoNkogGzbjI" ,"11/11/2022","Analisis Combinatorio 5","Binomio de Newton")
 
+//apuntes teoricos
 
 const apunteMT1 = new ApuntesT(1,"11FKyBjjHMXqiwRs5N_zYokG_3fizW4-p","23/08/2022","Logica 1","Logica, Formulas Proposicionales, Leyes Logicas","Teorico",1,1,"1-19")
 const apunteMT2 = new ApuntesT(2,"1nPrMsmmqCMc2N2qItmjnnD4PgW49k5-h","30/08/2022","Logica 2","Razonamientos y sus metodos","Teorico",[2,3,4,5],1,"19-22")
@@ -114,19 +123,23 @@ const apunteMT17 = new ApuntesT(17,"1Ck3d3cvsSCrhA0ItPgy1is6Jagttpkht","14/10/20
 const apunteMT18 = new ApuntesT(18,"1WZNSFBxw_CsVrlUu87anmiv3GcPxrPYJ","18/10/2022","Funciones 2","Igualdad de funciones, tipos de funciones","Teorico",26,5,"Todos")
 const apunteMT19 = new ApuntesT(19,"1LfPeY_zuMNa_wTCcUsM5qiTuy09rRraU","21/10/2022","Funciones 3","Composicion de Funciones","Teorico",27,"","")
 const apunteMT20 = new ApuntesT(20,"1sd_J_gPSZ8Xn3_yH7gXq2n7LqRihvdIJ","21/10/2022","Funicones 4","Funcion Inversa","Teorico",28,"","")
-const apunteMT22 = new ApuntesT(22,"16ZXZzcv2Gem90uv57m5kt6fjH5w6_QiT","11/11/2022","COmbinatorai 1", "Conbiantoria","Teorico",null,null,null)
+const apunteMT21 = new ApuntesT(21,"1oonCS17_AgHduA453DBb8avra_9DDe7F","01/11/2022","Induccion Matematica","Induccion Matematica","Teorico",null,null,null)
+const apunteMT22 = new ApuntesT(22,"16ZXZzcv2Gem90uv57m5kt6fjH5w6_QiT","11/11/2022","Combinatoria 1", "Conbinatoria","Teorico",null,null,null)
 
+
+// apuntes practico
 const apunteMP1 = new ApuntesP(1,"12NetAvDgn7barmTUuDTFfJkD5M8Ifz0R/","23/08/2022","Logica TP","Logica /Razonamientos / Formulas Proposicionales","Practico")
 const apunteMP2 = new ApuntesP(2,"1s5rky_xxbQ3SBTHv30Gg-IUU5cIH53Nf","09/09/2022","Conjuntos TP","Teoria de conjuntos / Union / Interseccion","Practico")
 const apunteMP3 = new ApuntesP(3,"1EcjjSxA4_-3S0RSBoNkwCfv9SuLR1pyS","23/09/2022","Relaciones","Relaciones entre conjuntos, binaria","Practico")
 const apunteMP4 = new ApuntesP(4,"18JhaU9aENk9copCSBgA3TcuKkT06YzgH","18/10/2022","Funciones TP","Funciones, composicion, inversa","Practico")
 const apunteMP5 = new ApuntesP(5,"1L_z2Xrgrdpj1tZoZ4knWpxU17sD9-HGT","18/10/2022","Funciones TP 2","Ejercictacion funciones","Practico")
+const apunteMP6 = new ApuntesP(6,"1xAQgjCS5flpcCZcX_orCwK2WEHxncm0c","01/11/2022","Induccion Matematica","Induccion Matematica","Practico")
 const apunteMP7 = new ApuntesP(7,"1tM2vmxB2zLc9-2EhoWRplbvIxhbKRnAz","11/11/2022","Combinatoria", "Combinatoria","Practico")
 
 
-matematica1.videos.push(videoM1,videoM2,videoM3,videoM4,videoM5, videoM6,videoM7, videoM8, videoM9, videoM10, videoM11,videoM12,videoM13, videoM14,videoM15, videoM16,videoM17, videoM18,videoM19, videoM20,videoM21,videoM22, videoM23, videoM24, videoM25,videoM26,videoM27,videoM28)
-matematica1.apuntesteoricos.push(apunteMT1,apunteMT2,apunteMT3,apunteMT4,apunteMT5,apunteMT6, apunteMT7,apunteMT8,apunteMT9,apunteMT10,apunteMT11,apunteMT12, apunteMT13, apunteMT14, apunteMT15,apunteMT16, apunteMT17, apunteMT18,apunteMT19, apunteMT20, apunteMT22)
-matematica1.apuntespracticos.push(apunteMP1,apunteMP2, apunteMP3, apunteMP4, apunteMP5, apunteMP7)
+matematica1.videos.push(videoM1,videoM2,videoM3,videoM4,videoM5, videoM6,videoM7, videoM8, videoM9, videoM10, videoM11,videoM12,videoM13, videoM14,videoM15, videoM16,videoM17, videoM18,videoM19, videoM20,videoM21,videoM22, videoM23, videoM24, videoM25,videoM26,videoM27,videoM28, videoM29,videoM30,videoM31,videoM32,videoM33,videoM34)
+matematica1.apuntesteoricos.push(apunteMT1,apunteMT2,apunteMT3,apunteMT4,apunteMT5,apunteMT6, apunteMT7,apunteMT8,apunteMT9,apunteMT10,apunteMT11,apunteMT12, apunteMT13, apunteMT14, apunteMT15,apunteMT16, apunteMT17, apunteMT18,apunteMT19, apunteMT20,apunteMT21 ,apunteMT22)
+matematica1.apuntespracticos.push(apunteMP1,apunteMP2, apunteMP3, apunteMP4, apunteMP5,apunteMP6, apunteMP7)
 
 //INTRODUCCIONPROGRAMACION
 const introduccionProgramacion = new Materias("Inpr","Introducción a la programación",8,"1BEQTT_9Lisq-PX6dEAGoclC6Nr1TjGF8","1n0fOy_9yfjjdQorwG4X_ZN1iOUSoO1XK",`<a href ="https://gobstones.github.io/gobstones-jr/?course=gobstones/curso-InPr-UNQ" target = "_blanck">Entorno Gobstone JR</a> ,<a href="https://gobstones.github.io/gobstones-sr/?course=gobstones/curso-InPr-UNQ" target= "_blank">Entorno Gobstone SR</a>`,"De las 8 hrs semanales se vuelven 6 ya que las dos correspondientes a los miercoles, es para que practiques en tu casa, igual se reservan ese dia y horario para que el examen sea ese dia. Examen en papel aunque todas las clases son en computadora, Si no sabes programar hasta parametros es relativamente facil despues de eso se complica un poco, Si sabes programar, lo que mas te va a costar son los contratos, la sintaxis y algunas cosas raras de gobstone. Clases totalmente invertidas, es decir, la forma de la clase es: indagacion, teoria y luego practica, la cual en clase presencial la idea es que sea solo practica, aunque los profesores te responden cualquier consulta que tengas, los contenidos se van desbloqueando semana a semana y tienen un campus virtual distinto al de la facultad (2022 mi caso)")
@@ -189,7 +202,9 @@ const videoT1 = new Videos(1,"hyYz3tKSPPA","25/08/2022","Historia del sistema Un
 const videoT2 = new Videos(2, "muF3fBNqFJs","03/09/2022", "Historia del sistema Universitario 2", "Ley Avellaneda (1820-1905)")
 const videoT3 = new Videos(3,"pc6XeTwhAhk","08/08/2022","Historia del sistema Universitario 3","La Reforma Universitaria (1918-1930)")
 const videoT4 = new Videos(4,"61ju4wPcJMc","15/09/2022","Historia Del sistema Universitario 4","Universidad y Trabajo (1946-1955)")
-const videoT5 = new Videos(5,"8mTSzd0C1EM","13/10/2022","Laica o Libre","Laica o Libre")
+const videoT5 = new Videos(5,"8mTSzd0C1EM","13/10/2022","Historia del sistema Universitario","Laica o Libre")
+const videoT6 = new Videos(6,"nULmeM7K508","21/10/2022","Hsitoria del sistema Universitario","Universidad y dictadura")
+
 
 const apunteT1 = new ApuntesT(1,"1augRuBhwtNDwIdI72mruY-BSN1Cx4V6v","25/08/2022","Clase 1","Origenes medievales de la universidad","Teorico",1,null,null)
 const apunteT2 = new ApuntesT(2,"1L3hfJaEeXpGxm3j5qbYpiOufTGDUTSVY","03/09/2022","Clase 2","De 1820 a 1900, Ley de Avellaneda","Teorico",[2,3],null,null)
